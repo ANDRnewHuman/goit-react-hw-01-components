0a1dc63f9c74b970fe1965265f037fb955d7229c
ANDRnewHuman <blsckcollapse@gmail.com>
2023-05-27 07:42:03 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
-export const App = () => {
+import './App.css';
+import PropTypes from 'prop-types';
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/FriendList';
+import friends from '../components/Pr';
+import FriendListItem from './FriendList/FriendListItem';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import data from './data.json';
+import user from './user.json';
+import transactions from './transactions.json';
+
+function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics stats={data} />
+      <FriendList>
+        {friends.map(friend => (
+          <FriendListItem
+            key={friend.id}
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+            id={friend.id}
+          />
+        ))}
+      </FriendList>
+      <TransactionHistory items={transactions} />;
     </div>
   );
-};
+}
+
+export default App;
